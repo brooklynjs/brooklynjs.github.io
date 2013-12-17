4b6ecc424f984417b5fd179a8da7ac90e88afd47
Jed Schmidt <where@jed.is>
2013-12-17 17:38:14 -0500

    better spacing for longer numbers

--- budget.js
+++ budget.js
@@ -1,21 +1,21 @@
 module.exports =
 
-  0     // budget.js - keeping the books for @brooklyn_js
-        // by @jedschmidt and @brianloveswords
+  0      // budget.js - keeping the books for @brooklyn_js
+         // by @jedschmidt and @brianloveswords
 
-        // BrooklynJS #1, 2013-11-14T19:00Z at @61Local
-        // featuring @nakajima, @jandet, @SaraJChipps, and @jeresig
+         // BrooklynJS #1, 2013-11-14T19:00Z at @61Local
+         // featuring @nakajima, @jandet, @SaraJChipps, and @jeresig
 
-+ 360   // ticket sales  @peatix
-+ 350   // sponsorship   @github
++ 360    // ticket sales  @peatix
++ 350    // sponsorship   @github
 
-- 360   // bar tab       @61local
-- 81    // tickets       @peatix
-- 51    // banners       @BuildASign
-- 50    // venue         @61local
-- 32    // VGA adapter   @amazon
-- 21    // nametags      @staples
-- 14    // domain        @namecheap
+- 360    // bar tab       @61local
+- 81     // tickets       @peatix
+- 51     // banners       @BuildASign
+- 50     // venue         @61local
+- 32     // VGA adapter   @amazon
+- 21     // nametags      @staples
+- 14     // domain        @namecheap
 
-- 50.50 // donation      @GirlsWhoCode
-- 50.50 // donation      @ScriptEdnyc
+- 50.50  // donation      @GirlsWhoCode
+- 50.50  // donation      @ScriptEdnyc
