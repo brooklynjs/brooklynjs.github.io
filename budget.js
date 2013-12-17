b4af0ecd742393ef10498071ed6e05a93ab06ad7
Jed Schmidt <where@jed.is>
2013-12-17 18:23:09 -0500

    tests

--- budget.js
+++ budget.js
@@ -1,7 +1,9 @@
-module.exports = (
+process.exit(
 
     0.00 // budget.js - keeping the books for @brooklyn_js
          // by @jedschmidt and @brianloveswords
+
+         // cat budget.js | node -p | xargs -0 printf 'balance = %s'
 
          // BrooklynJS #1, 2013-11-14T19:00Z at @61Local
          // featuring @nakajima, @jandet, @SaraJChipps, and @jeresig
@@ -22,6 +24,7 @@ module.exports = (
 
          // BrooklynJS #2, 2013-12-16T19:00Z at @61Local
          // featuring @tbuchok, @vinceallenvince, @buritica, @benjamn, and @reconbot
+         // with a surprise appearance by @jedschmidt's barbershop quartet
 
 + 665.00 // ticket sales  @peatix
 + 450.00 // sponsorship   @crushlovely
@@ -36,4 +39,4 @@ module.exports = (
 - 105.70 // donation      @GirlsWhoCode
 - 105.70 // donation      @ScriptEdnyc
 
-).toFixed(2)
+)
