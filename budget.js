e42149b87c42346f9d86bbcc2bcd37bc687e6171
Jed Schmidt <where@jed.is>
2013-12-17 18:08:03 -0500

    prettify

--- budget.js
+++ budget.js
@@ -1,37 +1,37 @@
 module.exports = (
 
-  0      // budget.js - keeping the books for @brooklyn_js
+    0.00 // budget.js - keeping the books for @brooklyn_js
          // by @jedschmidt and @brianloveswords
 
          // BrooklynJS #1, 2013-11-14T19:00Z at @61Local
          // featuring @nakajima, @jandet, @SaraJChipps, and @jeresig
 
-+ 360    // ticket sales  @peatix
-+ 350    // sponsorship   @github
++ 360.00 // ticket sales  @peatix
++ 350.00 // sponsorship   @github
 
-- 360    // bar tab       @61local
-- 81     // tickets       @peatix
-- 51     // banners       @BuildASign
-- 50     // venue         @61local
-- 32     // VGA adapter   @amazon
-- 21     // nametags      @staples
-- 14     // domain        @namecheap
+- 360.00 // bar tab       @61local
+-  81.00 // tickets       @peatix
+-  51.00 // banners       @BuildASign
+-  50.00 // venue         @61local
+-  32.00 // VGA adapter   @amazon
+-  21.00 // nametags      @staples
+-  14.00 // domain        @namecheap
 
-- 50.50  // donation      @GirlsWhoCode
-- 50.50  // donation      @ScriptEdnyc
+-  50.50 // donation      @GirlsWhoCode
+-  50.50 // donation      @ScriptEdnyc
 
          // BrooklynJS #2, 2013-12-16T19:00Z at @61Local
          // featuring @tbuchok, @vinceallenvince, @buritica, @benjamn, and @reconbot
 
-+ 665    // ticket sales  @peatix
-+ 450    // sponsorship   @crushlovely
++ 665.00 // ticket sales  @peatix
++ 450.00 // sponsorship   @crushlovely
 
 - 644.38 // bar tab       @61local
-- 107    // tickets       @peatix
-- 65.18  // stickers      @diginate
-- 50     // venue         @61local
-- 29.55  // VGA cable     @amazon
-- 7.49   // paper tickets @amazon
+- 107.00 // tickets       @peatix
+-  65.18 // stickers      @diginate
+-  50.00 // venue         @61local
+-  29.55 // VGA cable     @amazon
+-   7.49 // paper tickets @amazon
 
 - 105.70 // donation      @GirlsWhoCode
 - 105.70 // donation      @ScriptEdnyc
