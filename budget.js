5eb119ae357c4619d3c4cd8dc79f72be7b9500d6
Jed Schmidt <where@jed.is>
2014-01-23 10:02:40 -0500

    add numbers from brooklyn js the third

--- budget.js
+++ budget.js
@@ -1,43 +1,59 @@
 process.exit(
 
-    0.00 // budget.js - keeping the books for @brooklyn_js
-         // by @jedschmidt and @brianloveswords
+     0.00 // budget.js - keeping the books for @brooklyn_js
+	  // by @jedschmidt and @brianloveswords
 
-         // for details, see our accountant:
-         // https://travis-ci.org/brooklynjs/brooklynjs.github.io
+	  // for details, see our accountant:
+	  // https://travis-ci.org/brooklynjs/brooklynjs.github.io
 
-         // BrooklynJS #1, 2013-11-14T19:00Z at @61Local
-         // featuring @nakajima, @jandet, @SaraJChipps, and @jeresig
+	  // BrooklynJS #1, 2013-11-14T19:00Z at @61Local
+	  // featuring @nakajima, @jandet, @SaraJChipps, and @jeresig
 
-+ 360.00 // ticket sales  @peatix
-+ 350.00 // sponsorship   @github
++  360.00 // ticket sales  @peatix
++  350.00 // sponsorship   @github
 
-- 360.00 // bar tab       @61local
--  81.00 // tickets       @peatix
--  51.00 // banners       @BuildASign
--  50.00 // venue         @61local
--  32.00 // VGA adapter   @amazon
--  21.00 // nametags      @staples
--  14.00 // domain        @namecheap
+-  360.00 // bar tab       @61local
+-   81.00 // ticketing     @peatix
+-   51.00 // banners       @BuildASign
+-   50.00 // venue         @61local
+-   32.00 // VGA adapter   @amazon
+-   21.00 // nametags      @staples
+-   14.00 // domain        @namecheap
 
--  50.50 // donation      @GirlsWhoCode
--  50.50 // donation      @ScriptEdnyc
+-   50.50 // donation      @GirlsWhoCode
+-   50.50 // donation      @ScriptEdnyc
 
-         // BrooklynJS #2, 2013-12-16T19:00Z at @61Local
-         // featuring @tbuchok, @vinceallenvince, @buritica, @benjamn, and @reconbot
-         // with a surprise appearance by @jedschmidt's barbershop quartet
+	  // BrooklynJS #2, 2013-12-16T19:00Z at @61Local
+	  // featuring @tbuchok, @vinceallenvince, @buritica, @benjamn, and @reconbot
+	  // with special musical guest, @jedschmidt's barbershop quartet, the four fives
 
-+ 665.00 // ticket sales  @peatix
-+ 450.00 // sponsorship   @crushlovely
++  665.00 // ticket sales  @peatix
++  450.00 // sponsorship   @crushlovely
 
-- 644.38 // bar tab       @61local
-- 107.00 // tickets       @peatix
--  65.18 // stickers      @diginate
--  50.00 // venue         @61local
--  29.55 // VGA cable     @amazon
--   7.49 // paper tickets @amazon
+-  644.38 // bar tab       @61local
+-  107.00 // ticketing     @peatix
+-   65.18 // stickers      @diginate
+-   50.00 // venue         @61local
+-   29.55 // VGA cable     @amazon
+-    7.49 // paper tickets @amazon
 
-- 105.70 // donation      @GirlsWhoCode
-- 105.70 // donation      @ScriptEdnyc
+-  105.70 // donation      @GirlsWhoCode
+-  105.70 // donation      @ScriptEdnyc
 
+	  // BrooklynJS #3, 2014-01-22T19:00Z at @61Local
+	  // featuring @thlorenz, @donohoe, @mathisonian, and @jennschiffer
+	  // with special musical guest, @csvsoundsystem
+
++  970.00 // ticket sales  @peatix
++ 1500.00 // sponsorship   @nodejitsu, @digitalocean, and @harvest
+
+-  800.00 // bar tab       @61local
+-  136.99 // ticketing     @peatix
+-  237.57 // tokens        @CuboNewYork521
+-   79.59 // stickers      @diginate
+-   50.00 // venue         @61local
+-   10.99 // 3.5mm cable   @amazon
+
+-  577.43 // donation      @GirlsWhoCode
+-  577.43 // donation      @ScriptEdnyc
 )
